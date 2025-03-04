1f1a1a0731bfbccf8dc0b4525a8b9789780adfde
TenCerak <cerovsky2@gmail.com>
2025-03-04 20:48:02 +0100

    přidání sentry

--- AnotherWeatherApp/MauiProgram.cs
+++ AnotherWeatherApp/MauiProgram.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Maui.Extensions;
 using CommunityToolkit.Maui.Markup;
 using CommunityToolkit.Maui;
+using CommunityToolkit.Mvvm;
 
 namespace AnotherWeatherApp;
 
@@ -10,18 +11,28 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseMauiCommunityToolkitMarkup()
-			.UseMauiCommunityToolkit()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkitMarkup()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .UseSentry(options =>
+            {
+                options.Dsn = "https://b342aeab536f6de9ea0cc578e4c78dc1@o4508921182617600.ingest.de.sentry.io/4508921184911440";
+
+                #if DEBUG
+                options.Debug = true;
+                #endif
+
+                options.TracesSampleRate = 1.0;
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
