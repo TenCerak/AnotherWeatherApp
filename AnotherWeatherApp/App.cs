2e7fec65b00e97b9e474479831883bd83f7de886
TenCerak <cerovsky2@gmail.com>
2025-03-04 19:47:45 +0100

    odstranění xaml

---
+++ AnotherWeatherApp/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherWeatherApp
+{
+    public partial class App : Application
+    {
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Windows[0].Page = new MainPage();
+            return Windows[0];
+        }
+    }
+}
