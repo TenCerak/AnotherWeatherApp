c579e698ad64ad26d6465afab9f803dbbab691d1
TenCerak <cerovsky2@gmail.com>
2025-03-22 09:38:36 +0100

    start stránka

---
+++ AnotherWeatherApp/Models/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AnotherWeatherApp.Interfaces;
+using AnotherWeatherApp.ViewModels.Base;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public partial class MainPageViewModel : BaseViewModel
+{
+    public MainPageViewModel(IAnalyticsService analyticsService, IDispatcher dispatcher) : base(analyticsService, dispatcher)
+    {
+    }
+
+    [RelayCommand]
+    public void TrackEvent()
+    {
+        AnalyticsService.Track("MainPageViewModel.TrackEvent");
+    }
+}
+
---
+++ AnotherWeatherApp/Pages/MainPage.cs
@@ -0,0 +1,22 @@
+using AnotherWeatherApp.Interfaces;
+using AnotherWeatherApp.Pages.Base;
+
+namespace AnotherWeatherApp.ViewPages;
+
+public class MainPage : BaseContentPage<MainPageViewModel>
+{
+	public MainPage(MainPageViewModel model, IAnalyticsService analyticsService) : base(model, analyticsService)
+    {
+
+		Content = new VerticalStackLayout
+		{
+
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				},
+				new Label {Text = "Funguje to?"},
+			}
+		};
+
+	}
+}  
