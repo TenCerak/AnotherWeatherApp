95d757ea92f33da3a05ed82b68b83d8229ee1325
TenCerak <cerovsky2@gmail.com>
2025-03-22 10:34:06 +0100

    konfigurace sentry

--- AnotherWeatherApp/MauiProgram.cs
+++ AnotherWeatherApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Sentry.Maui;
 using CommunityToolkit.Maui.Extensions;
 using CommunityToolkit.Maui.Markup;
 using CommunityToolkit.Maui;
@@ -10,24 +11,14 @@ using System.Text.Json;
 using AnotherWeatherApp.ViewPages;
 using AnotherWeatherApp.ViewModels.Base;
 using AnotherWeatherApp.Pages.Base;
+using System.Diagnostics;
 
 namespace AnotherWeatherApp;
 
 public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
-	{
-        var secretsFile = Path.Combine(FileSystem.AppDataDirectory, "secrets.json");
-        string sentryDSN = string.Empty;
-        if (File.Exists(secretsFile))
-        {
-            var json = File.ReadAllText(secretsFile);
-            var secrets = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
-            
-            if(secrets is not null)
-                sentryDSN = secrets["SentryDSN"];
-        }
-
+    { 
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
@@ -41,13 +32,7 @@ public static class MauiProgram
             })
             .UseSentry(options =>
             {
-                options.Dsn = sentryDSN;
-
-                #if DEBUG
-                options.Debug = true;
-                #endif
-
-                options.TracesSampleRate = 1.0;
+                ConfigureSentryOptions(options);
             });
 
 #if DEBUG
@@ -74,5 +59,37 @@ public static class MauiProgram
     where TViewModel : BaseViewModel
     {
         return services.AddTransientWithShellRoute<TPage, TViewModel>(AppShell.GetPageRoute<TPage>());
+    }
+    static void ConfigureSentryOptions(SentryMauiOptions options)
+    {
+        options.TracesSampleRate = 1.0;
+        options.IncludeTextInBreadcrumbs = true;
+        options.IncludeTitleInBreadcrumbs = true;
+        options.IncludeBackgroundingStateInBreadcrumbs = true;
+        options.StackTraceMode = StackTraceMode.Enhanced;
+        options.IsGlobalModeEnabled = true;
+
+        options.TracesSampleRate = 1.0;
+        options.ProfilesSampleRate = 1.0;
+
+
+        ConfigureDebugSentryOptions(options);
+        ConfigureReleaseSentryOptions(options);
+
+        [Conditional("DEBUG")]
+        static void ConfigureDebugSentryOptions(SentryMauiOptions options)
+        {
+            options.Dsn = "https://b342aeab536f6de9ea0cc578e4c78dc1@o4508921182617600.ingest.de.sentry.io/4508921184911440";
+            options.Debug = true;
+            options.Environment = "DEBUG";
+            options.DiagnosticLevel = SentryLevel.Debug;
+        }
+
+        [Conditional("RELEASE")]
+        static void ConfigureReleaseSentryOptions(SentryMauiOptions options)
+        {
+            options.Dsn = "https://b342aeab536f6de9ea0cc578e4c78dc1@o4508921182617600.ingest.de.sentry.io/4508921184911440";
+            options.Environment = "RELEASE";
+        }
     }
 }
--- AnotherWeatherApp/Pages/MainPage.cs
+++ AnotherWeatherApp/Pages/MainPage.cs
@@ -7,7 +7,7 @@ public class MainPage : BaseContentPage<MainPageViewModel>
 {
 	public MainPage(MainPageViewModel model, IAnalyticsService analyticsService) : base(model, analyticsService)
     {
-
+ 
 		Content = new VerticalStackLayout
 		{
 
@@ -15,7 +15,8 @@ public class MainPage : BaseContentPage<MainPageViewModel>
 				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
 				},
 				new Label {Text = "Funguje to?"},
-			}
+				new Button {Text = "Sentry test", Command = new Command( () => { SentrySdk.CaptureMessage("Hello sentry"); }) }
+            }
 		};
 
 	}
