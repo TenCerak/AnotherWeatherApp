01100725be435383ea7b6a867eaf07067494227f
TenCerak <cerovsky2@gmail.com>
2025-03-22 09:38:08 +0100

    base model a content page

---
+++ AnotherWeatherApp/Models/Base/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using AnotherWeatherApp.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherWeatherApp.ViewModels.Base
+{
+    public abstract class BaseViewModel(IAnalyticsService analyticsService,IDispatcher dispatcher) : ObservableObject
+    {
+        protected IDispatcher Dispatcher { get; } = dispatcher;
+        protected IAnalyticsService AnalyticsService { get; } = analyticsService;
+    }
+}
---
+++ AnotherWeatherApp/Pages/Base/BaseContentPage.cs
@@ -0,0 +1,43 @@
+﻿using AnotherWeatherApp.Interfaces;
+using AnotherWeatherApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherWeatherApp.Pages.Base
+{
+    public abstract class BaseContentPage : ContentPage
+    {
+        protected IAnalyticsService AnalyticsService { get; }
+        protected BaseContentPage(in IAnalyticsService analyticsService)
+        {
+            AnalyticsService = analyticsService;
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            AnalyticsService.Track($"{GetType().Name} appeared");
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            AnalyticsService.Track($"{GetType().Name} disappeared");
+        }
+    }
+
+
+    public  abstract class BaseContentPage<T> : BaseContentPage where T : BaseViewModel
+    {
+        protected T ViewModel { get; }
+        protected BaseContentPage(in T viewModel, in IAnalyticsService analyticsService)
+            : base(analyticsService)
+        {
+            base.BindingContext = viewModel;
+        }
+    }
+}
